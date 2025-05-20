d4ebca5f982d407865758635c20c25f4efb8d2a0
Mark0sDev <awsknows@outlook.com>
2025-05-20 11:47:05 +0300

    Update For 20.05.2025-11-47

--- api/getRefferal.js
+++ api/getRefferal.js
@@ -1,55 +1,52 @@
+import { Bot } from 'grammy'
 
-const { Bot } = require('grammy');
+const bot = new Bot('7974451879:AAF2xuRDdaAt-hNf_9GwN3GNhPBcWDBlzJk')
 
+export default async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type")
 
-export default function handler(req, res) {
-    const bot = new Bot('7974451879:AAF2xuRDdaAt-hNf_9GwN3GNhPBcWDBlzJk');
-
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-    const { userId } = req.query;
+    const { userId } = req.query
 
     if (!userId) {
-        return res.status(400).json({ error: 'Query' });
+        return res.status(400).json({ error: 'Query' })
     }
 
     const inlineQueryResult = {
         id: '1',
         parse_mode: 'HTML',
-        caption: '🐏 <b>Legendary game in telegram - PLAY AND EARN TON</b> 🕹️', // Подпись к фото
-        type: 'photo', // Тип результата - фото
-        photo_url: 'https://bot.sven-ton.com/img/playnow.jpg', // URL фото
-        thumbnail_url: 'https://bot.sven-ton.com/img/playnow.jpg', // Миниатюра фото
+        caption: '🐏 <b>Legendary game in telegram - PLAY AND EARN TON</b> 🕹️',
+        type: 'photo',
+        photo_url: 'https://bot.sven-ton.com/img/playnow.jpg',
+        thumbnail_url: 'https://bot.sven-ton.com/img/playnow.jpg',
         reply_markup: {
             inline_keyboard: [
                 [
                     {
                         text: '▶️ Play Now',
-                        url: `https://t.me/SvenTon_bot?start=${userId}` // Ссылка на бота
-                    }
-                ]
-            ]
-        }
-    };
+                        url: `https://t.me/SvenTon_bot?start=${userId}`,
+                    },
+                ],
+            ],
+        },
+    }
 
+    try {
+        const preparedInlineMessage = await bot.api.savePreparedInlineMessage(
+            userId,
+            inlineQueryResult,
+            {
+                allow_user_chats: true,
+                allow_bot_chats: false,
+                allow_group_chats: true,
+                allow_channel_chats: false,
+            }
+        )
 
-    // Предположим, что мы хотим отправить сообщение подготовленному пользователю
-    const preparedInlineMessage = bot.api.savePreparedInlineMessage(
-        userId,
-        inlineQueryResult,
-        {
-            allow_user_chats: true,
-            allow_bot_chats: false,
-            allow_group_chats: true,
-            allow_channel_chats: false,
-        }
-    );
-
-
-    // Отправляем ответ клиенту
-    res.json({
-        data: preparedInlineMessage,
-    });
+        res.json({ data: preparedInlineMessage })
+    } catch (error) {
+        console.error('Error:', error)
+        res.status(500).json({ error: 'Internal error' })
+    }
 }
